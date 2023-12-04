cf1f5578da32553eb0806404ab5172985e25bf69
NguynNguyn02 <trungnguyenna02@gmail.com>
2023-12-05 06:22:55 +0700

    update

---
+++ WebBanHang/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHang.Models;
+using WebBanHang.Models.EF;
+
+namespace WebBanHang.Areas.Admin.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: Admin/News
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            var items = db.News.OrderByDescending(x => x.CreatedDate).ToList();
+            return View(items);
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(News model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                model.CategoryId = 18;
+                model.ModifiedDate = DateTime.Now;
+                model.Alias = WebBanHang.Models.Common.Filter.FilterChar(model.Title);
+                db.News.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+
+    }
+}
--- WebBanHang/Models/EF/News.cs
+++ WebBanHang/Models/EF/News.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace WebBanHang.Models.EF
 {
@@ -19,6 +20,7 @@ namespace WebBanHang.Models.EF
         public string Alias { get; set; }
 
         public string Description { get; set; }
+        [AllowHtml]
         public string Detail { get; set; }
         public string Image { get; set; }
         public int CategoryId { get; set; }
