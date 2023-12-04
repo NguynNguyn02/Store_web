96425a5c51f518405729ec942cb541680364ee69
NguynNguyn02 <trungnguyenna02@gmail.com>
2023-12-05 00:00:21 +0700

    update

--- WebBanHang/Areas/Admin/Controllers/CategoryController.cs
+++ WebBanHang/Areas/Admin/Controllers/CategoryController.cs
@@ -64,5 +64,17 @@ namespace WebBanHang.Areas.Admin.Controllers
             }
             return View(model);
         }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = db.Categories.Find(id);
+            if (item != null)
+            {
+                db.Categories.Remove(item);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
     }
 }
